2c07826e87f194d5e1a5695dd478576c203dd9af
alex <alex@plshark.net>
2020-03-08 20:55:13 -0400

    Add project for migrating database

---
+++ db/src/main/resources/db/migration/postgres/V202003071721__init.sql
@@ -0,0 +1,22 @@
+SET search_path TO ${schema};
+
+DO $$
+    BEGIN
+        CREATE USER ${username} PASSWORD '${password}';
+    EXCEPTION WHEN DUPLICATE_OBJECT THEN
+        RAISE NOTICE 'not creating existing user';
+    END
+$$;
+GRANT USAGE ON SCHEMA ${schema} TO ${username};
+
+-- data tables
+CREATE TABLE restaurant
+(
+    id             BIGSERIAL PRIMARY KEY,
+    name           VARCHAR(256)             NOT NULL,
+    container_type VARCHAR(128)             NOT NULL,
+    create_time    TIMESTAMP WITH TIME ZONE NOT NULL
+);
+GRANT USAGE ON restaurant_id_seq TO ${username};
+CREATE INDEX restaurant_name_idx ON restaurant (name);
+GRANT SELECT, INSERT, UPDATE, DELETE ON restaurant TO ${username};
