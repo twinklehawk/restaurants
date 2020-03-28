4bab78040b89ea2f95715eb32d9ab1637b52aa54
alex <alex@plshark.net>
2020-03-27 22:32:13 -0400

    Use kotlin notation for dependencies

--- restaurants-api/build.gradle.kts
+++ restaurants-api/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 dependencies {
     implementation(enforcedPlatform(project(":platform")))
     api("io.projectreactor:reactor-core")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("io.mockk:mockk:1.9.3")
