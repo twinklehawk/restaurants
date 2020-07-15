7a18748ae55c49db610768aec2f82d0bfc129580
alex <alex@plshark.net>
2020-07-15 14:38:35 -0400

    Remove wildcard imports

--- restaurants-service/src/main/kotlin/net/plshark/restaurant/controller/RestaurantsController.kt
+++ restaurants-service/src/main/kotlin/net/plshark/restaurant/controller/RestaurantsController.kt
@@ -5,7 +5,15 @@ import net.plshark.restaurant.exception.NotFoundException
 import net.plshark.restaurant.Restaurant
 import net.plshark.restaurant.RestaurantsService
 import net.plshark.restaurant.repository.RestaurantsRepository
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
