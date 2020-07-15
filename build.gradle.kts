7d91b3ad0f5b80fc0609cc67c9ad575bd1005626
alex <alex@plshark.net>
2020-07-15 14:17:30 -0400

    Set up dependency updates plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    id("org.springframework.boot") version "2.2.5.RELEASE" apply false
     kotlin("jvm") version "1.3.61" apply false
     kotlin("plugin.spring") version "1.3.61" apply false
+    id("org.springframework.boot") version "2.2.5.RELEASE" apply false
+    id("com.github.ben-manes.versions") version "0.28.0"
 }
 
 allprojects {
@@ -12,4 +13,9 @@ allprojects {
     }
     group = "net.plshark.takeoutcontainers"
     version = "0.0.1"
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    checkConstraints = true
+    gradleReleaseChannel = "current"
 }
