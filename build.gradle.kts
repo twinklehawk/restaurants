ee67cbfe7307837454db28e18be4a171dbfbe134
alex <alex@plshark.net>
2020-07-15 14:23:00 -0400

    Update kotlin plugins from 1.3.61 to 1.3.72

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.3.61" apply false
-    kotlin("plugin.spring") version "1.3.61" apply false
+    kotlin("jvm") version "1.3.72" apply false
+    kotlin("plugin.spring") version "1.3.72" apply false
     id("org.springframework.boot") version "2.2.5.RELEASE" apply false
     id("com.github.ben-manes.versions") version "0.28.0"
 }
