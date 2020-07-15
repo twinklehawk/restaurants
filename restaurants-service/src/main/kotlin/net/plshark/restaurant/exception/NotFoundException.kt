77ff71dbef29d74cc15aa168e052a9716306f9c9
alex <alex@plshark.net>
2020-07-15 14:41:17 -0400

    Fix magic number warning

--- restaurants-service/src/main/kotlin/net/plshark/restaurant/exception/NotFoundException.kt
+++ restaurants-service/src/main/kotlin/net/plshark/restaurant/exception/NotFoundException.kt
@@ -1,6 +1,9 @@
 package net.plshark.restaurant.exception
 
-class NotFoundException(message: String? = null, cause: Throwable? = null) : HttpServerException(404, message, cause) {
+import org.springframework.http.HttpStatus
+
+class NotFoundException(message: String? = null, cause: Throwable? = null) :
+    HttpServerException(HttpStatus.NOT_FOUND.value(), message, cause) {
     constructor() : this(null, null)
     constructor(message: String?) : this(message, null)
     constructor(cause: Throwable?) : this(null, cause)
